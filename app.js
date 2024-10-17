5d0eafdb3c6c40438a715985ba3810796bff8ce5
prem17299 <55232029+prem17299@users.noreply.github.com>
2024-10-18 04:13:24 +0530

    Episode2

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const heading = React.createElement("h1",
-{ id:"heading"}, 
-"Hello from namaste React !"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div",{id:"parent"},[ 
 React.createElement("div",{id:"child1"},
@@ -14,6 +12,5 @@ React.createElement("div",{id:"child2"},
   ])
 ]
 )
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
 # Namaste React Course
+
+# parcel
+- Dev build
+- Local server
+- HMR - Hot module replacement
+- File watching algorithm in c++
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consistency Hashing
+- Code spilting
+- Differential Bundling- support older browsers 
+- Error handling
+- Diagnostics
+- http
+- tree shaking- remove unused code 
+- different prod and dev bundles
